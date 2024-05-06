c62b308046b7ae15d424a1d64275cb1488270109
marinodilauro <m.dilauro90@gmail.com>
2024-05-06 15:22:37 +0200

    Add style and navbar to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,42 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Google font link -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- CSS -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
   <title>laravel-primi-passi</title>
 </head>
 
 <body>
+
+  <nav id="navbar">
+    <div class="button-container">
+
+      <button class="button">
+        <a href="">Home</a>
+      </button>
+
+      <button class="button">
+        <a href="">About</a>
+      </button>
+
+      <button class="button">
+        <a href="">Portfolio</a>
+      </button>
+
+      <button class="button">
+        <a href="">Contact me</a>
+      </button>
+
+    </div>
+  </nav>
+
 
   <h1>Hello World</h1>
 
